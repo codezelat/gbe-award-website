54ab1075fe980cb672d3a0f0398ce7406e24e7bd
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2026-06-23 03:43:48 +0530

    minor fixes and txt changes

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,11 +1,11 @@
 export const assetPaths = {
-  logo: "/assets/live/logo-e1738704108777-195x300-33756ccd0b.png",
-  favicon: "/assets/live/cropped-gbe-awards-logo-192x192-37b43e1197.png",
-  trophy: "/assets/live/trophy-4x-1-png-06b29f01c0.webp",
-  award: "/assets/live/award-small-img-300x300-png-bea10b6124.webp",
-  interview: "/assets/live/interview-768x419-png-00a21f1268.webp",
-  awardInHand: "/assets/live/award-in-hand-2-768x419-png-3eb6fc4a8c.webp",
-  redCarpet: "/assets/live/red-carpet-night-city-red-carpet-city-768x512-jpg-6e2aceb8e5.webp",
+  logo: "/assets/brand/gbe-logo.png",
+  favicon: "/assets/brand/favicon-192.png",
+  trophy: "/assets/brand/trophy.webp",
+  award: "/assets/brand/award-icon.webp",
+  interview: "/assets/journey/interview.webp",
+  awardInHand: "/assets/journey/award-in-hand.webp",
+  redCarpet: "/assets/journey/red-carpet.webp",
 };
 
 export const navItems = [
@@ -44,72 +44,72 @@ export const featuredNominees = [
   {
     title: "Best Political Program On Digital Media 2024",
     name: "Colombo Times Round Table",
-    image: "/assets/live/ct-logo-300x129-jpeg-a38dd4a961.webp",
+    image: "/assets/nominees/colombo-times.webp",
   },
   {
     title: "The Most Popular News Program Of The Year",
     name: "Siyatha Tele Wakiya ( Siyatha TV) - Mr.Lal Hemantha Mawalaga",
-    image: "/assets/live/352204755_6229089260515005_3656344635379458924_n-300x182-png-6a143025a8.webp",
+    image: "/assets/nominees/siyatha-tele-wakiya.webp",
   },
   {
     title: "The Best Female News Anchor",
     name: "Miss.Ruwanika Kumaradasa",
-    image: "/assets/live/whatsapp-image-2025-02-05-at-11-08-34-300x300-jpeg-c0ae82ebb5.webp",
+    image: "/assets/nominees/ruwanika-kumaradasa.webp",
   },
   {
     title: "The Best Premium Japanese Lubricants & Auxiliary Fluids",
     name: "GS.Lubricants Limited-TOTACHI",
-    image: "/assets/live/386598295_719341140210924_8993629143531363977_n-300x300-jpg-d8acbe10ad.webp",
+    image: "/assets/nominees/gs-lubricants-totachi.webp",
   },
   {
     title: "The Best Digital Marketing Icon Of The Year",
     name: "The Platform Daddy (Pvt) Ltd",
-    image: "/assets/live/455245847_500933525816546_5718242030144361530_n-300x300-jpg-ca336be98c.webp",
+    image: "/assets/nominees/platform-daddy.webp",
   },
   {
     title: "The Best Female Sport Presenterin",
     name: "Ms.Ashika Farzan",
-    image: "/assets/live/whatsapp-image-2025-02-05-at-11-08-34-300x300-jpeg-c0ae82ebb5.webp",
+    image: "/assets/nominees/ruwanika-kumaradasa.webp",
   },
   {
     title: "The Best Television presenter – Female",
     name: "Ms.Eranga Kawshalya",
-    image: "/assets/live/472860999_1701282890466218_9148892402642118520_n-300x300-jpg-c7311cd913.webp",
+    image: "/assets/nominees/eranga-kawshalya.webp",
   },
   {
     title: "The Most Popular Political Program Of The Year",
     name: "(Hiru TV) Salakuna",
-    image: "/assets/live/405675188_676285417966699_8516845023254613228_n-225x300-jpg-58b1a65adc.webp",
+    image: "/assets/nominees/hiru-tv-salakuna.webp",
   },
   {
     title: "Best Political Program On Digital Media 2024",
     name: "Colombo Times Round Table",
-    image: "/assets/live/ct-logo-300x129-jpeg-a38dd4a961.webp",
+    image: "/assets/nominees/colombo-times.webp",
   },
   {
     title: "The Most Popular News Program Of The Year",
     name: "Siyatha Tele Wakiya ( Siyatha TV) - Mr.Lal Hemantha Mawalaga",
-    image: "/assets/live/352204755_6229089260515005_3656344635379458924_n-300x182-png-6a143025a8.webp",
+    image: "/assets/nominees/siyatha-tele-wakiya.webp",
   },
   {
     title: "The Best Female News Anchor",
     name: "Miss.Ruwanika Kumaradasa",
-    image: "/assets/live/whatsapp-image-2025-02-05-at-11-08-34-300x300-jpeg-c0ae82ebb5.webp",
+    image: "/assets/nominees/ruwanika-kumaradasa.webp",
   },
   {
     title: "The Best Premium Japanese Lubricants & Auxiliary Fluids",
     name: "GS.Lubricants Limited-TOTACHI",
-    image: "/assets/live/386598295_719341140210924_8993629143531363977_n-300x300-jpg-d8acbe10ad.webp",
+    image: "/assets/nominees/gs-lubricants-totachi.webp",
   },
   {
     title: "The Best Digital Marketing Icon Of The Year",
     name: "The Platform Daddy (Pvt) Ltd",
-    image: "/assets/live/455245847_500933525816546_5718242030144361530_n-300x300-jpg-ca336be98c.webp",
+    image: "/assets/nominees/platform-daddy.webp",
   },
   {
     title: "The Best Female Sport Presenterin",
     name: "Ms.Ashika Farzan",
-    image: "/assets/live/whatsapp-image-2025-02-05-at-11-08-34-300x300-jpeg-c0ae82ebb5.webp",
+    image: "/assets/nominees/ruwanika-kumaradasa.webp",
   },
 ];
 
@@ -117,77 +117,77 @@ export const nomineeEntries = [
   {
     title: "Nominee To Award For The Best Entrepreneur Of The Year",
     name: "Deshamanya Sandun Dolamulla",
-    image: "/assets/live/411175861_859981076135968_8796758621214014077_n-1-300x300-jpg-ea7f8483af.webp",
+    image: "/assets/nominees/sandun-dolamulla.webp",
   },
   {
     title: "Nominee To Award For The Best Real Estate Solution Provide",
     name: "Sevenseas Group of Company",
-    image: "/assets/live/301678592_509915237802378_6501660839522904986_n-300x298-jpg-c240f50f20.webp",
+    image: "/assets/nominees/sevenseas-group.webp",
   },
   {
     title: "Nominee To Award For The Most Talented Television News Presenter Of The Year",
     name: "Monara Tv - Mr. Rasika Sadaruwan Idirimanna",
-    image: "/assets/live/whatsapp-image-2025-02-05-at-11-08-34-1-300x300-jpeg-03fa053375.webp",
+    image: "/assets/nominees/monara-tv.webp",
   },
   {
     title: "Nominee To Award For The Most Popular & Best Japanese Language School In Sri Lanka",
     name: "Apesense Japanese Language Academy",
-    image: "/assets/live/447771910_420314107490413_4311184715465234628_n-300x212-jpg-68d2a01728.webp",
+    image: "/assets/nominees/apesense-academy.webp",
   },
   {
     title: "Nominee To Award For The Best Commercial Production Company 2024",
     name: "Vanilla Lanka (Pvt) Ltd",
-    image: "/assets/live/279763667_1028465911109740_2763943975644703878_n-300x300-jpg-a1d2b98e9b.webp",
+    image: "/assets/nominees/vanilla-lanka.webp",
   },
   {
     title: "Nominee To Award For The Best Enterprourner of The Year",
     name: "Athugala Credit & Investments (Pvt) Ltd",
-    image: "/assets/live/411175861_859981076135968_8796758621214014077_n-1-300x300-jpg-ea7f8483af.webp",
+    image: "/assets/nominees/sandun-dolamulla.webp",
   },
   {
     title: "Nominee To Award For The Customer Service Leadership Award",
     name: "SDS Weighing System",
-    image: "/assets/live/whatsapp-image-2025-02-05-at-11-08-34-1-300x300-jpeg-03fa053375.webp",
+    image: "/assets/nominees/monara-tv.webp",
   },
   {
     title: "Nominee To Award For The Most Emerging Real Estate Company",
     name: "Athugala Credit& Investments (Pvt) Ltd",
-    image: "/assets/live/411175861_859981076135968_8796758621214014077_n-1-300x300-jpg-ea7f8483af.webp",
+    image: "/assets/nominees/sandun-dolamulla.webp",
   },
   {
     title: "Nominee To Award For The Best Entrepreneur Of The Year",
     name: "Deshamanya Sandun Dolamulla",
-    image: "/assets/live/411175861_859981076135968_8796758621214014077_n-1-300x300-jpg-ea7f8483af.webp",
+    image: "/assets/nominees/sandun-dolamulla.webp",
   },
   {
     title: "Nominee To Award For The Best Real Estate Solution Provide",
     name: "Sevenseas Group of Company",
-    image: "/assets/live/301678592_509915237802378_6501660839522904986_n-300x298-jpg-c240f50f20.webp",
+    image: "/assets/nominees/sevenseas-group.webp",
   },
   {
     title: "Nominee To Award For The Most Talented Television News Presenter Of The Year",
     name: "Monara Tv - Mr. Rasika Sadaruwan Idirimanna",
-    image: "/assets/live/whatsapp-image-2025-02-05-at-11-08-34-1-300x300-jpeg-03fa053375.webp",
+    image: "/assets/nominees/monara-tv.webp",
   },
   {
     title: "Nominee To Award For The Most Popular & Best Japanese Language School In Sri Lanka",
     name: "Apesense Japanese Language Academy",
-    image: "/assets/live/447771910_420314107490413_4311184715465234628_n-300x212-jpg-68d2a01728.webp",
+    image: "/assets/nominees/apesense-academy.webp",
   },
   {
     title: "Nominee To Award For The Best Commercial Production Company 2024",
     name: "Vanilla Lanka (Pvt) Ltd",
-    image: "/assets/live/279763667_1028465911109740_2763943975644703878_n-300x300-jpg-a1d2b98e9b.webp",
+    image: "/assets/nominees/vanilla-lanka.webp",
   },
   {
     title: "Nominee To Award For The Best Enterprourner of The Year",
     name: "Athugala Credit & Investments (Pvt) Ltd",
-    image: "/assets/live/411175861_859981076135968_8796758621214014077_n-1-300x300-jpg-ea7f8483af.webp",
+    image: "/assets/nominees/sandun-dolamulla.webp",
   },
 ];
 
 export const seo = {
-  title: "Global Business Excellence Awards 2025 | GBE - Recognizing Global Leaders",
+  title: "Global Business Excellence Awards 2026 | GBE - Recognizing Global Leaders",
   description:
-    "Join the Global Business Excellence Awards 2025 (GBE Awards 2025) and celebrate outstanding achievements in business. Recognizing global innovation, leadership, and excellence. Nominate now!",
+    "Join the Global Business Excellence Awards 2026 (GBE Awards 2026) and celebrate outstanding achievements in business. Recognizing global innovation, leadership, and excellence. Nominate now!",
 };
